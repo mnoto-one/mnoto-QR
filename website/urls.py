2b01e5e1c1590cfbc191d1dba6e114c0711ef197
Pablo Noori <pablo@noori.com>
2023-06-07 07:40:08 +0330

    update

--- website/urls.py
+++ website/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'website'
 
 urlpatterns = [
+    path('', views.home_view, name='home'),
     path('generate-qr/', views.generate_qr_with_logo, name='generate_qr'),
     path('qr-code-generated/', views.qr_code_generated, name='qr_code_generated'),
 ]
